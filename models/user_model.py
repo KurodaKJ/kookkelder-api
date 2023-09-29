731019866446e696910af70547f25f3f66b604ff
KurodaKJ <jack.kaeorahan@student.gildeopleidingen.nl>
2023-09-29 13:15:14 +0200

    Create user models for samples

---
+++ models/user_model.py
@@ -0,0 +1,15 @@
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    user_type_id = db.Column(db.Integer, db.ForeignKey('user_types.id'), nullable=False)
+    
+    
+class UserType(db.Model):
+    __tablename__ = 'user_types'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    type = db.Column(db.String(50), unique=True, nullable=False)
