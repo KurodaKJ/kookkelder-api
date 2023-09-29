bc29ceca00e1baf966c724386a76c586eb549087
KurodaKJ <jack.kaeorahan@student.gildeopleidingen.nl>
2023-09-29 12:51:41 +0200

    Add required folders for MVC structures

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:3306/kookkelder'
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
